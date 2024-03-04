5a84838fd91ade4171b1924d0a614ac856a23ef6
Amanda Brenden <amandalb@uia.no>
2024-03-04 14:28:26 +0000

    gjort gdt /(mangler navn endringer)

--- src/4_SAL/src/gdt.c
+++ src/4_SAL/src/gdt.c
@@ -34,18 +34,10 @@ idt_ptr_t   idt_ptr;
 
 
 
-// Initialisation routine - zeroes all the interrupt service routines,
-// initialises the GDT and IDT.
-void init_descriptor_tables()
-{
-   // Initialise the global descriptor table.
-   init_gdt();
-}
-
 static void init_gdt()
 {
    gdt_ptr.limit = (sizeof(gdt_entry_t) * 5) - 1;
-   gdt_ptr.base  = (u32int)&gdt_entries;
+   gdt_ptr.base  = (uint32_t)&gdt_entries;
 
    gdt_set_gate(0, 0, 0, 0, 0);                // Null segment
    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF); // Code segment
@@ -53,11 +45,11 @@ static void init_gdt()
    gdt_set_gate(3, 0, 0xFFFFFFFF, 0xFA, 0xCF); // User mode code segment
    gdt_set_gate(4, 0, 0xFFFFFFFF, 0xF2, 0xCF); // User mode data segment
 
-   gdt_flush((u32int)&gdt_ptr);
+   gdt_flush((uint32_t)&gdt_ptr);
 }
 
 // Set the value of one GDT entry.
-static void gdt_set_gate(s32int num, u32int base, u32int limit, u8int access, u8int gran)
+static void gdt_set_gate(sint32_t num, uint32_t base, uint32_t limit, uint8_t access, uint8_t gran)
 {
    gdt_entries[num].base_low    = (base & 0xFFFF);
    gdt_entries[num].base_middle = (base >> 16) & 0xFF;
